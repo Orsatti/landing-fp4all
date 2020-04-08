7fe85c9d15a7491af7bce5a011ea586c3138ad99
fernando <fernando.orsatti@gmail.com>
2020-04-08 19:42:26 -0300

    integracao com back

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,53 @@
 function toggleNav() {
     document.getElementById('header-nav').classList.toggle('header__nav--is-show');
 }
+
+document.getElementById("button").addEventListener("click", function (event) {
+    event.preventDefault()
+});
+
+$(document).ready(function () {
+    document.getElementById("button").addEventListener("click", function (event) {
+        event.preventDefault()
+    });
+});
+
+
+var EnviarEmail = function () {
+    debugger;
+    var form = $("#form-dados");
+    form.validate();
+    if (form.valid()) {
+        //var dado = form.serialize();
+        var dado = {};
+        dado.name = $("#psfp-signup-name").val();
+        dado.email = $("#psfp-signup-email").val();
+        dado.senha = $("#psfp-signup-pass").val();
+        dado = JSON.stringify(dado);
+        $.ajax({
+            url: "http://localhost:64568/api/v0/pessoas/autoregister",
+            type: 'POST',
+            data: dado,
+            //dataType: "json",
+            success: function (data) {
+                // TODO Fazer aparecer o retorno de sucesso
+                $("#sucesso").addClass("alert--is-show");
+            },
+            error: function (data) {
+                if (data.status === 400)
+                {
+                    if (data.responseJSON.Message == "Email já cadastrado.")
+                    {
+                        $("#error-email").html("E-mail já cadastrado")
+                        $("#error-email").removeClass("field-validation-valid");  
+                    }
+                }
+                else
+                {
+                    // TODO Erro desconhecido
+                }
+            },
+            contentType: "application/json",
+        });
+    }
+}
